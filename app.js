09dd8830c4a79e251799511bc690c90808a6c8e3
GOVIND <work.govindsahu@gmail.com>
2025-12-30 16:42:29 +0530

    complete config driven setup

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import crypto from "crypto";
 import { spawn } from "child_process";
+import fs from "fs/promises";
+import yaml from "js-yaml";
 
 const app = express();
 
@@ -9,7 +11,7 @@ const PORT = 4000;
 
 app.use(express.json());
 
-app.post("/deploy", (req, res, next) => {
+app.post("/deploy", async (req, res, next) => {
   console.log("webhook started!");
   try {
     const GitHubSignature = req.headers["x-hub-signature-256"];
@@ -27,6 +29,34 @@ app.post("/deploy", (req, res, next) => {
 
     res.end("OK");
 
+    const ref = req.body.ref;
+    const branch = ref.split("/").pop();
+
+    let yamlContent = await fs.readFile("workspace.yml", "utf8");
+
+    // Replace ${VARIABLE} with process.env.VARIABLE
+    yamlContent = yamlContent.replace(/\${(\w+)}/g, (match, key) => {
+      return process.env[key] || match;
+    });
+
+    const { projects } = yaml.load(yamlContent);
+
+    await fs.writeFile("deploy.sh", "");
+
+    const repository = projects.find(
+      (project) => project.name === req.body.repository.name
+    );
+
+    if (repository.branch !== branch) {
+      return res.status(400).json({
+        error: `Server is setup for ${repository.branch}, not for ${branch}`,
+      });
+    }
+
+    for (const key in repository.commands) {
+      await fs.appendFile("deploy.sh", `${repository.commands[key]}\n`);
+    }
+
     const bashChildProcess = spawn("bash", [`./deploy.sh`]);
 
     bashChildProcess.stdout.on("data", (data) => {
@@ -37,8 +67,9 @@ app.post("/deploy", (req, res, next) => {
       process.stderr.write(data);
     });
 
-    bashChildProcess.on("close", (code) => {
+    bashChildProcess.on("close", async (code) => {
       if (code === 0) {
+        await fs.writeFile("deploy.sh", "");
         console.log("Script executed successfully!");
       } else {
         console.log("Script execution failed!");
